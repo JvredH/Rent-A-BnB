35108d4adbf5b532993c283cefc75868c1e30a72
jared <hem.jared@gmail.com>
2022-12-19 14:55:50 -0800

    updated requireAuth function

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -54,15 +54,19 @@ const restoreUser = (req, res, next) => {
 
 
 // If there is no current user, return an error
-const requireAuth = function (req, _res, next) { // going to have to use this at some time, so learn it
-  if (req.user) return next();
+const requireAuth = [
+  restoreUser,
+  function (req, _res, next) { // going to have to use this at some time, so learn it
+    if (req.user) return next();
 
-  const err = new Error('Authentication required');
-  err.title = 'Authentication required';
-  err.errors = ['Authentication required'];
-  err.status = 401;
-  return next(err);
-}
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = ['Authentication required'];
+    err.status = 401;
+    return next(err);
+  }
+]
+
 
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
