2e2b4970fa4f041979433704e9a0f634bd87612a
jared <hem.jared@gmail.com>
2022-12-21 23:26:16 -0800

    create a review based on spots id

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -43,8 +43,24 @@ const validationSpots = (req, res, next) => {
   next();
 }
 
+const validationReviews = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors
+    .array()
+    .map((error) => `${error.msg}`);
+
+      const err = Error('Validation Error');
+      err.errors = errors;
+      err.status = 400;
+      next(err)
+  }
+  next()
+}
 
 module.exports = {
   handleValidationErrors,
-  validationSpots
+  validationSpots,
+  validationReviews
 };
