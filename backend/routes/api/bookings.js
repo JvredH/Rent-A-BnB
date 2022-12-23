6d56349fb72ce55adace096ab903b5270c637531
jared <hem.jared@gmail.com>
2022-12-22 22:01:23 -0800

    edit a spot, still needs some testing

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,10 +1,29 @@
 const express = require('express');
 
-
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Booking, Spot, SpotImage } = require('../../db/models')
+const { check } = require('express-validator');
+const { validationBookings } = require('../../utils/validation');
+
+const { Op } = require('sequelize')
 
 const router = express.Router();
+
+const validateBookings = [
+  check('startDate')
+    .custom((value) => {
+      const date = new Date(value);
+      return!isNaN(date);
+    })
+    .withMessage('startDate must be a valid date'),
+  check('endDate')
+    .custom((value) => {
+      const date = new Date((value));
+      return !isNaN(date);
+    })
+    .withMessage('endDate must be a valid date'),
+  validationBookings
+]
 
 // get all of the current user's bookings
 router.get('/current', requireAuth, async ( req, res, next ) => {
@@ -45,6 +64,78 @@ router.get('/current', requireAuth, async ( req, res, next ) => {
   }
 
   return res.json({Bookings: bookingsJson})
+})
+
+// edit a booking
+router.put('/:bookingId', requireAuth, validateBookings, async ( req, res, next ) => {
+  const { bookingId } = req.params;
+  const user = req.user;
+  const { startDate, endDate } = req.body;
+
+  const booking = await Booking.findByPk(bookingId);
+
+  if(!booking) {
+    res.status(404);
+    return res.json({
+      message: "Booking couldn't be found",
+      statusCode: res.statusCode
+    })
+  }
+
+  if (user.id !== booking.userId) {
+    res.status(401);
+    res.json({
+      message: 'Only booking creator can edit a booking'
+    })
+  }
+
+  if (endDate < startDate) {
+    res.status(400);
+    return res.json({
+      message: "Validation error",
+      statusCode: res.statusCode,
+      errors: {
+        endDate: 'endDate cannot come before startDate'
+      }
+    })
+  }
+
+  if (endDate < new Date()) {
+    res.status(403);
+    return res.json({
+      message: "Past bookings can't be modified",
+      statusCode: res.statusCode
+    })
+  }
+
+  const bookingConflictCheck = await Booking.findAll({
+    where: {
+      spotId: booking.spotId,
+      [Op.or]: {
+        startDate: {[Op.between]: [startDate, endDate]},
+        endDate: {[Op.between]: [startDate, endDate]}
+      }
+    }
+  })
+
+  if (bookingConflictCheck.length) {
+    res.status(403);
+    return res.json({
+      message: "Sorry, this spot is already booked for the specified dates",
+      statusCode: res.statusCode,
+      errors: {
+        startDate: "Start date conflicts with an existing booking",
+        endDate: "End date conflicts with an existing booking"
+      }
+    })
+  }
+
+  booking.update({
+    startDate,
+    endDate
+  })
+
+  res.json(booking)
 })
 
 module.exports = router;
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -59,8 +59,25 @@ const validationReviews = (req, res, next) => {
   next()
 }
 
+const validationBookings = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors
+    .array()
+    .map((error) => `${error.msg}`);
+
+      const err = Error('Validation Error');
+      err.errors = errors;
+      err.status = 400;
+      next(err)
+  }
+  next()
+}
+
 module.exports = {
   handleValidationErrors,
   validationSpots,
-  validationReviews
+  validationReviews,
+  validationBookings
 };
