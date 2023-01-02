d5668d263e48998f43c96b79e867dc17ae01b530
jared <hem.jared@gmail.com>
2023-01-01 20:00:00 -0800

    some fixes made to sign up

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -10,7 +10,7 @@ const handleValidationErrors = (req, _res, next) => {
       .array()
       .map((error) => `${error.msg}`);
 
-    const err = Error('Bad request.');
+    const err = Error('Validation Error');
     err.errors = errors;
     err.status = 400;
     err.title = 'Bad request.';
