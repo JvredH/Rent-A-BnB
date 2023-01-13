fc6fd684b6329f6671c3fef35d3687b5821ef0ee
jared <hem.jared@gmail.com>
2023-01-13 15:04:59 -0800

    phase 4 login modal set up

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -17,14 +19,17 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <li>
-        <NavLink to="/login">Log In</NavLink>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
         <NavLink to="/signup">Sign Up</NavLink>
       </li>
     );
   }
 
   return (
-    <ul className='top-nav'>
+    <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
