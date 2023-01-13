c1e1b6428739d52d6cc9b134bcd392260965ea75
jared <hem.jared@gmail.com>
2023-01-13 14:41:31 -0800

    phase 3 drop down menu

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
       <li>
         <ProfileButton user={sessionUser} />
-        <button onClick={logout}>Log Out</button>
       </li>
     );
   } else {
@@ -32,7 +24,7 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
+    <ul className='top-nav'>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
